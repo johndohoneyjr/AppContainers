b7d5ccbacfeeeb53da939e8740537f46fa736a5b
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2022-04-26 05:32:26 -0700

    Added Hello entry point

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import json
 
 app = FastAPI()
 health_good = {"status": "healthy"}
+my_greeting = {"message": "Hello"}
 
 @app.get("/health")
 def health_check():
     return json.dumps(health_good)
+
+@app.get("/hello")
+def greeting():
+    return json.dumps(my_greeting)
 
 @app.get("/items/{item_id}")
 def item(item_id: int, q: Optional[str] = None):
