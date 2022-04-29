9d3a7f036edb3ee5be03093dc149fb4bfcb3123b
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2022-04-29 14:56:45 -0700

    Add Telemetry

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI
 import json
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+import time
+
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=a0a31278-af1a-4825-8e3d-226fbe8bcabe'))
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 health_good = {"status": "healthy"}
@@ -8,12 +15,17 @@ my_greeting = {"message": "Hello"}
 
 @app.get("/health")
 def health_check():
-    return json.dumps(health_good)
+    myData = "Time is: " + time.strftime('%a, %d %b %Y %H:%M:%S %Z(%z)') + " " + str(health_good)
+    logger.info(myData)
+    return json.dumps(myData)
 
 @app.get("/hello")
 def greeting():
-    return json.dumps(my_greeting)
+    myData = "Time is: " + time.strftime('%a, %d %b %Y %H:%M:%S %Z(%z)') + " " + str(my_greeting)
+    logger.info(myData)
+    return json.dumps(myData)
 
 @app.get("/items/{item_id}")
 def item(item_id: int, q: Optional[str] = None):
+    logger.info("item_id=" + item_id + " q=" + q)
     return {"item_id": item_id, "q": q}
